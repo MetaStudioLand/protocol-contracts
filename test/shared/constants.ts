0fb93d96f0a4e1fe0c37c7ed0a6620e2bd0de6a0
Frédéric PREVOST <fprevost@iorga.com>
2022-07-05 10:17:11 +0200

    Deleting unecessary role "ROLES_ADMIN_ROLE", replaced by standardised "DEFAULT_ADMIN_ROLE". Updating roles docs

--- test/shared/constants.ts
+++ test/shared/constants.ts
@@ -5,7 +5,8 @@ export const DATA = "0x42000000";
 export const RECEIVER_MAGIC_VALUE = "0x88a7ca5c";
 export const SPENDER_MAGIC_VALUE = "0x7b04a2d0";
 
-export const ROLES_ADMIN_ROLE = keccak256(toUtf8Bytes("ROLES_ADMIN_ROLE"));
+export const DEFAULT_ADMIN_ROLE =
+  "0x0000000000000000000000000000000000000000000000000000000000000000";
 export const PROXY_ROLE = keccak256(toUtf8Bytes("PROXY_ROLE"));
 export const FORWARDER_ROLE = keccak256(toUtf8Bytes("FORWARDER_ROLE"));
 export const PAUSER_ROLE = keccak256(toUtf8Bytes("PAUSER_ROLE"));
