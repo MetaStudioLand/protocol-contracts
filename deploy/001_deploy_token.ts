e61299ca990c8811831b62ad1a8fe8b61e06c957
Frédéric PREVOST <fprevost@iorga.com>
2022-07-18 17:32:36 +0200

    removing unused import

--- deploy/001_deploy_token.ts
+++ deploy/001_deploy_token.ts
@@ -1,7 +1,7 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
 import {ethers} from 'hardhat';
-import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/dist/src/signers';
+import {DeployFunction} from 'hardhat-deploy/types';
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const {deployments, getNamedAccounts} = hre;
   const {deployer, initialHolder} = await getNamedAccounts();
